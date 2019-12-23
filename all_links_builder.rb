7ff474a9dff117c9c84c62365f9a4a619ddee493
gnanasuriyan-gnanaoli <gnanasuriyan.gnanaoli@freshworks.com>
2019-12-23 23:27:00 +0530

    all links builder

---
+++ all_links_builder.rb
@@ -0,0 +1,28 @@
+require 'find'
+
+class AllLinksBuilder
+  def initialize(dir: Dir.pwd, file: 'all_links.html')
+    @dir = dir
+    @file = file
+  end
+
+  def write_to_file
+    File.open(@file, 'w') do |file|
+      file.write(html)
+    end
+  end
+
+  def html
+    html = "<ul>\n"
+    Find.find(@dir) do |path|
+      next unless path =~ /html\Z|md\Z/
+      href = path.gsub("#{@dir}/", '')
+      name = href.split('/').last
+      puts href
+      html = html+"\n<li><a href='#{href}' target='_blank'>#{href}</a></li>"
+    end
+    html = html+"\n</ul>"
+  end
+end
+
+AllLinksBuilder.new(dir: Dir.pwd, file: 'all_links.html').write_to_file
