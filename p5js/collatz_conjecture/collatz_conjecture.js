9fcc22e62a7d160a55760e7008f0295341390ca8
gnanasuriyan-gnanaoli <gnanasuriyan.gnanaoli@freshworks.com>
2020-06-25 23:20:31 +0530

    collatz conjecture

--- p5js/collatz_conjecture/collatz_conjecture.js
+++ p5js/collatz_conjecture/collatz_conjecture.js
@@ -4,9 +4,12 @@ var length;
 var currentAngle;
 var currentLength;
 function setup() {
-  createCanvas(400, 600);
+  createCanvas(400, 400);
+  
   angle_slider = createSlider(0.1, 1, 1, 0.01);
+  angle_slider.position(0, 0);
   length_slider = createSlider(1, 20, 20  , 1);
+  length_slider.position(width, 0);
   background(255);
   colorMode(HSB, 100);
   angle  = angle_slider.value();
@@ -20,17 +23,17 @@ function draw(){
     length = length_slider.value();
     angle = currentAngle;
     background(255);
-    for(m = 1; m <= 500; m++)
+    for(m = 1; m <= 1000; m++)
     {
       resetMatrix();
-      translate(0, height);
+      translate(0, height+20);
       collatz(m);
     }
   }
 }
 
 function collatz(n){
-  while(n != 1 ){
+  while(n != 1){
     if(n % 2 == 0){
       n = n / 2;
       turn('left');
