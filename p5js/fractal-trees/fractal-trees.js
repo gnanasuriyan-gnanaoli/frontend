843bdf649e6d1660bdf0b2b219528a766c4a8d5b
gnanasuriyan-gnanaoli <gnanasuriyan.gnanaoli@freshworks.com>
2020-06-27 01:14:25 +0530

    fractal trees with angle slider

--- p5js/fractal-trees/fractal-trees.js
+++ p5js/fractal-trees/fractal-trees.js
@@ -3,14 +3,16 @@ var angle;
 var fraction = 0.7;
 
 function setup(){
-  angle_slider = createSlider(0, TWO_PI, PI/4, 0.01);
-  createCanvas(600, 600);
-  
+  angle_slider = createSlider(0, TWO_PI, PI/12, 0.01);
+  angle_slider.style('width','300px');
+  createCanvas(600, 500);
+  stroke(255);
 }
 
 function draw(){
   background(0);
-  stroke(255);
+  textSize(12);
+  text('Angle (0 - 2 Pi) :'+angle_slider.value(), 0, height-3);
   angle = angle_slider.value();
   translate(width/2, height);
   branch(length);
