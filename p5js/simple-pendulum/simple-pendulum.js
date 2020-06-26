09667d5ac89b50008944bb2d4ae89cf78a7b9f21
gnanasuriyan-gnanaoli <gnanasuriyan.gnanaoli@freshworks.com>
2020-06-27 03:26:51 +0530

    pendulum changes

--- p5js/simple-pendulum/simple-pendulum.js
+++ p5js/simple-pendulum/simple-pendulum.js
@@ -2,29 +2,26 @@ var length = 200;
 var g = 1
 var t = 0
 function setup(){
-  x = createCanvas(450, 400);
-  x.center();
+  createCanvas(450, 400).center();
 }
 
 function draw(){
   background(244);
-  create_pendulum1(150, 'red');
-  create_pendulum1(200, 'blue');
-  create_pendulum1(100, 'green');
+  create_pendulum(150, 'red');
+  create_pendulum(200, 'blue');
+  create_pendulum(100, 'green');
   create_pendulum(200, 'yellow', PI/2);
 }
 
-function create_pendulum1(length, color){
-  create_pendulum(length, color, 1)
+function create_pendulum(length, color, max_angle = 1){
+  angular_displacement = sqrt(g/length);
+  angle_at_t = max_angle * cos(angular_displacement * t++/10);
+  x = width/2 + length * sin(angle_at_t);
+  y = length * cos(angle_at_t);
+  draw_pendulum(x,y, color)
 }
 
-function create_pendulum(length, color, max_angle){
-  angular_displacement = sqrt(g/length);
-  angle = max_angle * cos(angular_displacement * t++/10);
-
-  x = width/2 + length * sin(angle);
-  y = length * cos(angle);
-  
+function draw_pendulum(x,y, color){
   stroke(0, 111);
   line(width/2, 0, x, y);
   noStroke();
